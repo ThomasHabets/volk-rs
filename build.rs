a5c32eb3af0b72570be171c81b0a344c78359a89
Thomas Habets <thomas@habets.se>
2025-10-21 23:10:44 +0100

    Be ok with volk 2.0

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 fn main() {
     // Ensures Cargo links against libvolk and picks up the right search paths.
     pkg_config::Config::new()
-        .atleast_version("3.0") // adjust if needed
+        .atleast_version("2.0") // adjust if needed
         .probe("volk")
         .expect("Could not find VOLK via pkg-config");
 }

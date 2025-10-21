01bd565b38e321080a90444955322bbfc6fe8612
Thomas Habets <thomas@habets.se>
2025-10-21 22:08:38 +0000

    Working example

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,59 @@
-use libc::{c_float, c_uint};
+mod raw {
+    use libc::{c_float, c_uint};
+    #[link(name = "volk")]
+    unsafe extern "C" {
+        pub(crate) static mut volk_32f_sqrt_32f:
+            Option<extern "C" fn(out: *mut c_float, inp: *const c_float, len: c_uint)>;
+        // fn volk_malloc(size: usize, alignment: usize) -> *mut core::ffi::c_void;
+        // fn volk_free(ptr: *mut core::ffi::c_void);
+    }
+}
 
-#[link(name = "volk")]
-unsafe extern "C" {
-    fn volk_initialize();
-    // void volk_32f_sqr_32f(float* out, const float* in, unsigned int len);
-    fn volk_32f_sqrt_32f(out: *mut c_float, inp: *const c_float, len: c_uint);
-
-    // (Optional) aligned allocation helpers from VOLK:
-    // void* volk_malloc(size_t size, size_t alignment);
-    // void  volk_free(void* ptr);
-    fn volk_malloc(size: usize, alignment: usize) -> *mut core::ffi::c_void;
-    fn volk_free(ptr: *mut core::ffi::c_void);
+// Take square root of 32bit float.
+pub fn volk_32f_sqrt_32f(out: &mut [f32], inp: &[f32]) {
+    let func = unsafe { raw::volk_32f_sqrt_32f.unwrap() };
+    func(out.as_mut_ptr(), inp.as_ptr(), inp.len() as libc::c_uint)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn sqr() {
-        //unsafe { volk_initialize(); }
-        let input = vec![1.2f32];
-        let mut output = vec![0.0f32];
-        //unsafe { volk_32f_sqrt_32f(output.as_mut_ptr(), input.as_ptr(), input.len() as libc::c_uint ) };
-        unsafe {
-            let n = 1;
-                    let align = 64usize;
-            let in_ptr = volk_malloc(n * core::mem::size_of::<f32>(), align) as *mut c_float;
-        let out_ptr = volk_malloc(n * core::mem::size_of::<f32>(), align) as *mut c_float;
-        unsafe { volk_32f_sqrt_32f(out_ptr, in_ptr, input.len() as libc::c_uint ) };
+    trait MyAbs {
+        fn abs(self) -> f32;
+    }
+    impl MyAbs for f32 {
+        fn abs(self) -> f32 {
+            f32::abs(self)
         }
-        assert_eq!(output, vec![2.4f32]);
+    }
+
+    fn assert_close<T>(got: &[T], want: &[T])
+    where
+        T: MyAbs + Copy + PartialEq + std::fmt::Debug + std::fmt::Display,
+        T: std::ops::Sub<T>,
+        <T as std::ops::Sub>::Output: MyAbs,
+    {
+        assert_eq!(got.len(), want.len());
+        for (n, (g, w)) in got.iter().copied().zip(want.iter().copied()).enumerate() {
+            let diff = (g - w).abs();
+            assert!(
+                diff < 0.001,
+                "Comparison failed at entry {n}: diff between {g} and {w} is {diff}"
+            );
+        }
+    }
+
+    #[test]
+    fn test_volk_32f_sqrt_32f() {
+        for (input, want) in &[
+            (vec![4.0f32], vec![2.0f32]),
+            (vec![0.0f32, 1.0, 2.0, 4.0], vec![0.0, 1.0, 1.414213, 2.0]),
+        ] {
+            assert_eq!(input.len(), want.len());
+            let mut got = vec![0.0f32; want.len()];
+            volk_32f_sqrt_32f(&mut got, input);
+            assert_close(&got, want);
+        }
     }
 }

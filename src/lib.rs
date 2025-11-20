f9aaae5525dc7c644b1e9be1a6b335a618583ef7
Thomas Habets <thomas@habets.se>
2025-11-20 08:45:24 +0000

    Add crate docstrings

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+//! # volk-rs
+//! 
+//! Rust bindings for the volk library.
+//! 
+//! https://github.com/ThomasHabets/volk-rs
+//! 
+//! [VOLK][volk] is the Vector-Optimized Library of Kernels. It is a library
+//! that contains kernels of hand-written SIMD code for different mathematical
+//! operations. Since each SIMD architecture can be very different and no
+//! compiler has yet come along to handle vectorization properly or highly
+//! efficiently, VOLK approaches the problem differently.
+//! 
+//! [volk]: https://github.com/gnuradio/volk
 use num_complex::Complex;
 use paste::paste;
 
